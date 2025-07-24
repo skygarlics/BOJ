7e2f55b5250bcc3a0dfbdf433810b5f1797645c1
MANL <skygarlics@users.noreply.github.com>
2025-07-24 15:17:37 +0900

    LCS.py

---
+++ algos/PY/LCS.py
@@ -0,0 +1,34 @@
+import sys
+input = sys.stdin.readline
+
+def solve():
+    a = input().strip()
+    b = input().strip()
+
+    # LCS
+    n = len(a)
+    m = len(b)
+    dp = [[0] * (m + 1) for _ in range(n + 1)]
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            if a[i - 1] == b[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+    # Reconstruct LCS
+    lcs = []
+    i, j = n, m
+    while i > 0 and j > 0:
+        if a[i - 1] == b[j - 1]:
+            lcs.append(a[i - 1])
+            i -= 1
+            j -= 1
+        elif dp[i - 1][j] >= dp[i][j - 1]:
+            i -= 1
+        else:
+            j -= 1
+    lcs.reverse()
+    print(''.join(lcs))
+    
+solve()
