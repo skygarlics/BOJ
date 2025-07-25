ea3800bc8a45873d080965b602c0b9d8efdff253
MANL <skygarlics@users.noreply.github.com>
2025-07-25 18:12:19 +0900

    unionfind.py

---
+++ algos/PY/unionfind.py
@@ -0,0 +1,29 @@
+class UnionFind:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [0] * n
+
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+    
+    def union(self, x, y):
+        root_x = self.find(x)
+        root_y = self.find(y)
+
+        if root_x != root_y:
+            if self.rank[root_x] > self.rank[root_y]:
+                self.parent[root_y] = root_x
+            elif self.rank[root_x] < self.rank[root_y]:
+                self.parent[root_x] = root_y
+            else:
+                self.parent[root_y] = root_x
+                self.rank[root_x] += 1
+
+    def size(self, x):
+        root = self.find(x)
+        return sum(1 for i in range(len(self.parent)) if self.find(i) == root)
+    
+    def roots(self):
+        return {self.find(i) for i in range(len(self.parent))}
